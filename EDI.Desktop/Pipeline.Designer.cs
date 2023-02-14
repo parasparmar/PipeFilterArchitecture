bf31f40dfaf9e6dbaf9cbc95cb4907475a87dd33
Paras Parmar <parasparmar@gmail.com>
2023-02-14 13:49:43 +0530

    updated

---
+++ EDI.Desktop/Pipeline.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace EDI.Desktop
+{
+    partial class Pipeline
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pipeline));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.lblProcessPipeline = new System.Windows.Forms.ToolStripLabel();
+            this.ProcessPipelinebtn = new System.Windows.Forms.ToolStripButton();
+            this.tbLogs = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblProcessPipeline,
+            this.ProcessPipelinebtn});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // lblProcessPipeline
+            // 
+            this.lblProcessPipeline.Name = "lblProcessPipeline";
+            this.lblProcessPipeline.Size = new System.Drawing.Size(92, 22);
+            this.lblProcessPipeline.Text = "Process Pipeline";
+            // 
+            // ProcessPipelinebtn
+            // 
+            this.ProcessPipelinebtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ProcessPipelinebtn.Image = ((System.Drawing.Image)(resources.GetObject("ProcessPipelinebtn.Image")));
+            this.ProcessPipelinebtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ProcessPipelinebtn.Name = "ProcessPipelinebtn";
+            this.ProcessPipelinebtn.Size = new System.Drawing.Size(23, 22);
+            this.ProcessPipelinebtn.Text = "ProcessPipeline";
+            this.ProcessPipelinebtn.Click += new System.EventHandler(this.ProcessPipelinebtn_Click);
+            // 
+            // tbLogs
+            // 
+            this.tbLogs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbLogs.Location = new System.Drawing.Point(0, 25);
+            this.tbLogs.Multiline = true;
+            this.tbLogs.Name = "tbLogs";
+            this.tbLogs.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbLogs.Size = new System.Drawing.Size(800, 425);
+            this.tbLogs.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbLogs);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripLabel lblProcessPipeline;
+        private ToolStripButton ProcessPipelinebtn;
+        private TextBox tbLogs;
+    }
+}
---
+++ EDI.Desktop/Pipeline.cs
@@ -0,0 +1,37 @@
+using System.Windows.Forms;
+using EDI;
+namespace EDI.Desktop
+{
+    public partial class Pipeline : Form
+    {
+        public Pipeline()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProcessPipelinebtn_Click(object sender, EventArgs e)
+        {
+            tbLogs.Clear();
+            string Message = string.Empty;
+            //INPUT
+            IMessage message = new Message(
+                messageId: "1",
+                body: "This is the message body",
+                header: "Header Information",
+                subject: "Please set priority and encrypt the message");
+
+            //sending code to the pipeline and the message is encrypted and published into the queue  
+            SendPipeline sendPipeline = new SendPipeline(true, true);
+            var publishedMessage = sendPipeline.PerformOperation(message);
+            Message = $"Message Priority : {publishedMessage.IsMessagePrioritySet()} Message Encryption : {publishedMessage.IsMessageEncrypted()}";
+            tbLogs.Text = Message;
+
+            
+        }
+    }
+}
